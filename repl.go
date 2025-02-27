b793f3ace51d8524635d7bfadd5db905372b254b
saxton-chris <surfde22@gmail.com>
2025-02-27 10:39:52 -0600

    Created Help and Exit functionality

--- repl.go
+++ repl.go
@@ -8,19 +8,28 @@ import (
 )
 
 func startRepl() {
-	scanner := bufio.NewScanner(os.Stdin)
-
+	reader := bufio.NewScanner(os.Stdin)
 	for {
 		fmt.Print("Pokedex > ")
+		reader.Scan()
 
-		if scanner.Scan() {
-			words := cleanInput(scanner.Text())
+		words := cleanInput(reader.Text())
+		if len(words) == 0 {
+			continue
+		}
 
-			if len(words) == 0 {
-				continue
+		commandName := words[0]
+
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
 			}
-
-			fmt.Printf("Your command was: %s\n", words[0])
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
 		}
 	}
 }
@@ -29,4 +38,25 @@ func cleanInput(text string) []string {
 	output := strings.ToLower(text)
 	words := strings.Fields(output)
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
